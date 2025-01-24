ef797037022daba564693a2866afef1527df44bd
Adrien VH <adrien.vom-hoevel@niji.fr>
2025-01-24 09:54:10 +0100

    Navigation between pokemon detail

--- src/app/components/pokemon-detail/pokemon-detail.component.ts
+++ src/app/components/pokemon-detail/pokemon-detail.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule, TitleCasePipe } from '@angular/common';
-import { Component, Input, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { PokemonService } from 'src/app/services/pokemon.service';
 
 @Component({
@@ -12,23 +12,53 @@ import { PokemonService } from 'src/app/services/pokemon.service';
 })
 export class PokemonDetailComponent implements OnInit {
   pokemon: any;
+  pokemonList: any[] = []; 
+  currentPokemonIndex: number = 0; 
 
   constructor(
-    private route: ActivatedRoute, 
-    private pokemonService: PokemonService 
+    private route: ActivatedRoute,
+    private pokemonService: PokemonService,
+    private router: Router
   ) {}
 
   ngOnInit(): void {
-    const name = this.route.snapshot.paramMap.get('name');
-    if (name) {
-      this.pokemonService.getPokemonDetails(name).subscribe((data) => {
-        this.pokemon = data; // Stocker les données du Pokémon
+    this.pokemonService.getPokemons(150).subscribe((response) => {
+      this.pokemonList = response.results;
+
+      this.route.paramMap.subscribe((params) => {
+        const name = params.get('name');
+        if (name) {
+          this.currentPokemonIndex = this.pokemonList.findIndex(
+            (poke) => poke.name === name
+          );
+
+          this.getPokemonDetails(name);
+        }
       });
+    });
+  }
+
+  getPokemonDetails(name: string): void {
+    this.pokemonService.getPokemonDetails(name).subscribe((data) => {
+      this.pokemon = data;
+    });
+  }
+
+  navigateToPreviousPokemon(): void {
+    if (this.currentPokemonIndex > 0) {
+      const previousPokemon = this.pokemonList[this.currentPokemonIndex - 1];
+      this.router.navigate(['/pokemon', previousPokemon.name]);
+    }
+  }
+
+  navigateToNextPokemon(): void {
+    if (this.currentPokemonIndex < this.pokemonList.length - 1) {
+      const nextPokemon = this.pokemonList[this.currentPokemonIndex + 1];
+      this.router.navigate(['/pokemon', nextPokemon.name]);
     }
   }
 
   goBack(): void {
     window.history.back();
   }
-  
 }
