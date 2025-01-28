f554b95046380175cbb9a4de040c33a15021962b
Adrien VH <adrien.vom-hoevel@niji.fr>
2025-01-28 12:16:28 +0100

    Test building

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,11 +14,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'pokedex' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('pokedex');
-  });
+
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
